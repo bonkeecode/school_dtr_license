36b142b34bd84ffc60530e7de409b17b70bc1f7f
bonkeecode <103725413+bonkeecode@users.noreply.github.com>
2026-05-28 22:16:29 +0800

    update license

--- obj/Debug/net8.0-windows/SchoolDTR.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SchoolDTR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolDTR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96dd0f505c09b42e5222075152fc2bdc8030e751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9306e6777152904beb9786c626ea0cae121b181c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolDTR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolDTR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
